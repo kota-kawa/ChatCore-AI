35a0a6235de847d40be372db8134dacca0e28549
kota-kawa <kota7kawagoe@gmail.com>
2026-05-24 14:38:07 +0900

    Fix memo image migration revision

--- alembic/versions/20260524_03_drop_memo_image_url.py
+++ alembic/versions/20260524_03_drop_memo_image_url.py
@@ -1,7 +1,7 @@
 """Drop memo image URL field.
 
-Revision ID: 20260524_02
-Revises: 20260524_01
+Revision ID: 20260524_03
+Revises: 20260524_02
 Create Date: 2026-05-24 00:00:00
 """
 
@@ -9,8 +9,8 @@ from typing import Sequence, Union
 
 from alembic import op
 
-revision: str = "20260524_02"
-down_revision: Union[str, Sequence[str], None] = "20260524_01"
+revision: str = "20260524_03"
+down_revision: Union[str, Sequence[str], None] = "20260524_02"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
